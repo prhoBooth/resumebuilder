16e4234fe773acfcc4af41b0d03f6c1e97c4a2ca
Raghu Betina <raghu@firstdraft.com>
2016-11-23 00:44:31 +0000

    Whitelist columns in BulletTag for ActiveAdmin

--- app/admin/bullet_tag.rb
+++ app/admin/bullet_tag.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register BulletTag do
 
+ permit_params :bullet_id, :tag_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
