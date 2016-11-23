9c1dcc31872f4f45c5237ae7cf9ffcc6a9fc629d
Raghu Betina <raghu@firstdraft.com>
2016-11-23 00:44:51 +0000

    Only organization user can access show page

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,5 @@
 class OrganizationsController < ApplicationController
-  before_action :current_user_must_be_organization_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_organization_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_organization_user
     organization = Organization.find(params[:id])
