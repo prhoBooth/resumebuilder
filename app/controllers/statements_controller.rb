b2d407ff706ff849b5661eaf0e9fed66953b783a
Raghu Betina <raghu@firstdraft.com>
2016-11-23 00:44:51 +0000

    Only statement user can access show page

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -1,5 +1,5 @@
 class StatementsController < ApplicationController
-  before_action :current_user_must_be_statement_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_statement_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_statement_user
     statement = Statement.find(params[:id])
