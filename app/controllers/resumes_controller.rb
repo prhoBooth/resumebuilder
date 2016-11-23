c769f425691644a4be549fd0dc878847fb3a9c6d
Raghu Betina <raghu@firstdraft.com>
2016-11-23 00:44:51 +0000

    Only resume user can modify resume

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,4 +1,14 @@
 class ResumesController < ApplicationController
+  before_action :current_user_must_be_resume_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_resume_user
+    resume = Resume.find(params[:id])
+
+    unless current_user == resume.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @resumes = Resume.all
 
