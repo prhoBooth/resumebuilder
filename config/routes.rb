f1d73007794b38be875fa5344378dac1a21be6ae
Raghu Betina <raghu@firstdraft.com>
2016-11-23 00:44:08 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "organizations#index"
   # Routes for the Saved_statement resource:
   # CREATE
