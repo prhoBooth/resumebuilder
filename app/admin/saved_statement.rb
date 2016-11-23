01b9bcded4e878a8729bee52f8498d835d780c0b
Raghu Betina <raghu@firstdraft.com>
2016-11-23 00:44:51 +0000

    Whitelist columns in SavedStatement for ActiveAdmin

--- app/admin/saved_statement.rb
+++ app/admin/saved_statement.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register SavedStatement do
 
+ permit_params :statement_id, :resume_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
