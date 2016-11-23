8119c187751d77881395de1b82749decdce64ef8
Raghu Betina <raghu@firstdraft.com>
2016-11-23 00:43:50 +0000

    Direct association between users and statements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :statements,
+             :dependent => :destroy
+
   has_many   :organizations,
              :dependent => :destroy
 
