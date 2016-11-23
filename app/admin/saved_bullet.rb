40ba38998f51e44211c6f92eddb6925fbc853d43
Raghu Betina <raghu@firstdraft.com>
2016-11-23 00:44:45 +0000

    Whitelist columns in SavedBullet for ActiveAdmin

--- app/admin/saved_bullet.rb
+++ app/admin/saved_bullet.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register SavedBullet do
 
+ permit_params :bullet_id, :resume_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
